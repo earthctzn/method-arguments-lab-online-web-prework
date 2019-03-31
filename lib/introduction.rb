2d0a1df64f0aecee50b09b807b586847db16bff9
Caleb Aviles <earthctzn1@gmail.com>
2019-03-31 15:05:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction(name)
-  phrase = "Hi, my name is  #{name} and I am learning to program in Ruby."
+  phrase = "Hi, my name is  #{name}."
   puts phrase
 end
 
 def introduction_with_language(name, language)
-  phrase "Hi, my name is #{name} and I am learning to program in #{language}."
+  phrase = "Hi, my name is #{name} and I am learning to program in #{language}."
   puts phrase
 end
